96015f78d4aaee39f89e505676fb3efbe58d0a00
Nerly Ton <nerlyton@gmail.com>
2020-08-05 21:08:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,11 +11,11 @@ class CashRegister
     end
     
     def add_item(title, price, quantity = 1)
-      self.total += amount * quantity
+      self.total += price * quantity
     quantity.times do
       items << title
     end
-    self.last_transaction = amount * quantity
+    self.last_transaction = price * quantity
   end
   
   def apply_discount
